f7508ac87550790d87ac31f100143e212ee9286a
Srinivasan B R <srinivasan@Srinivasans-MacBook-Pro.local>
2025-10-13 16:25:01 +0530

    feat: Add Kubernetes deployment for microservices and CI script.

--- microservices/build.sh
+++ microservices/build.sh
@@ -10,8 +10,20 @@ DOCKER_USERNAME="${1:-}"
 DOCKER_TOKEN="${2:-}"
 SERVICES=("inventory" "payment" "gateway")
 IMAGE_TAG="latest"
-
+K8S_MANIFESTS=("deploy/inventory-deployment.yaml" "deploy/inventory-service.yaml" "deploy/payment-deployment.yaml" "deploy/payment-service.yaml" "deploy/gateway-deployment.yaml" "deploy/gateway-service.yaml")
 # --------------------------------------------------------------------------
+
+# --- FUNCTION FOR CROSS-PLATFORM SED ---
+sedi() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS (BSD sed) requires a backup extension (use "" for no backup)
+        sed -i "" "$@"
+    else
+        # Linux (GNU sed) does not require a backup extension argument
+        sed -i "$@"
+    fi
+}
+# ---------------------------------------
 
 # 1. Get Docker Username
 if [ -z "$DOCKER_USERNAME" ]; then
@@ -58,8 +70,45 @@ for SERVICE in "${SERVICES[@]}"; do
     echo "✅ Successfully pushed ${SERVICE} to Docker Hub."
 done
 
-# 6. Final Confirmation
+# 6. update K8s Manifests with Docker Username
+echo "--- 4. Preparing K8s Manifests"
+
+PLACEHOLDER="__DOCKER_USER__"
+
+for MANIFEST in "${K8S_MANIFESTS[@]}"; do
+  echo "   -> Updating image tag in ${MANIFEST}..."
+  if [[ "$MANIFEST" == *deployment.yaml ]]; then
+
+     echo "   -> Dynamically replacing $PLACEHOLDER with $DOCKER_USERNAME in $MANIFEST"
+
+     sedi -e "s#$PLACEHOLDER#$DOCKER_USERNAME#g" "$MANIFEST"
+     #sed -i "" -e "s#$PLACEHOLDER#$DOCKER_USERNAME#g" "$MANIFEST" 2>dev/null || \
+     #sed -i "s#$PLACEHOLDER#$DOCKER_USERNAME#g" "$MANIFEST"
+ fi
+done
+
+# 7. Apply K8s Manifests
+echo "--- 5. Applying Kubernetes Manifests ---"
+for MANIFEST in "${K8S_MANIFESTS[@]}"; do
+    echo "   -> Applying $MANIFEST..."
+     kubectl apply -f "$MANIFEST" # Uncomment this line when ready
+done
+
+# 8. Revert Changes (Crucial cleanup step)
+echo "--- 6. Reverting K8s Manifest changes ---"
+for MANIFEST in "${K8S_MANIFESTS[@]}"; do
+    if [[ "$MANIFEST" == *deployment.yaml ]]; then
+        echo "   -> Restoring placeholder in $MANIFEST"
+        # Reverse the sed command to put the placeholder back
+        sedi -e "s#$DOCKER_USERNAME#$PLACEHOLDER#g" "$MANIFEST"
+        #sed -i "" -e "s#$DOCKER_USERNAME#$PLACEHOLDER#g" "$MANIFEST" 2>/dev/null || \
+        #sed -i "s#$DOCKER_USERNAME#$PLACEHOLDER#g" "$MANIFEST"
+    fi
+done
+
+# 9. Final Confirmation
 echo "--------------------------------------------------------"
+echo "✅ DEPLOYMENT COMPLETE!"
 echo "✅ DOCKER BUILD & PUSH COMPLETE!"
 echo "Your images are now available in the ${DOCKER_USERNAME} repository."
 echo "--------------------------------------------------------"
